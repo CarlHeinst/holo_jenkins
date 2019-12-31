7993dc74395b8d40347f4dc26f43a90e25437f3e
Carl Heinst <cheinst@gmail.com>
2019-12-31 15:40:25 +0000

    Got vault working

--- extra-plugin.sh
+++ extra-plugin.sh
@@ -1,3 +1,3 @@
 ## If running on Windows ensure you have choco install jq
 # Otherwise it will fail.
-curl.exe -s -k "http://Admin:Admin@localhost:8080/pluginManager/api/json?depth=1" | jq -r '.plugins[].shortName' | tee plugins.txt
+curl.exe -s -k "http://admin:admin@localhost:8080/pluginManager/api/json?depth=1" | jq -r '.plugins[].shortName' | tee plugins.txt
