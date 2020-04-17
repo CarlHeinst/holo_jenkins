be821bc8fa824f7fed38ab24794c8618e1f39bab
Carl Heinst <cheinst@gmail.com>
2020-04-17 18:27:05 +0100

    Simplifying code and updating readme

--- scripts/dsl_build.groovy
+++ scripts/dsl_build.groovy
@@ -40,44 +40,6 @@ multibranchPipelineJob('REDEPLOY') {
     }
 }
 
-multibranchPipelineJob('OVT/OVT-CLONE3') {
-    branchSources {
-        git {
-            id = 'admin'
-            remote('https://github.com/tknerr/jenkins-pipes-helloworld.git')
-        }
-    }
-    factory {
-        workflowBranchProjectFactory {
-            scriptPath('Jenkinsfile')
-            }
-    }
-}
-
-multibranchPipelineJob('OVT/OVT-CLONE4') {
-    branchSources {
-        git {
-            id = 'admin'
-            remote('https://github.com/tknerr/jenkins-pipes-helloworld.git')
-        }
-    }
-    factory {
-        workflowBranchProjectFactory {
-            scriptPath('Jenkinsfile')
-            }
-    }
-}
-
-multibranchPipelineJob('OVT/OVT-CLONE5') {
-    branchSources {
-        git {
-            id = 'admin'
-            remote('https://github.com/tknerr/jenkins-pipes-helloworld.git')
-        }
-    }
-    factory {
-        workflowBranchProjectFactory {
-            scriptPath('Jenkinsfile')
-            }
-    }
-}
+folder('crap')
+folder('crap1')
+folder('crap2')
