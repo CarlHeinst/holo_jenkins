fe3c3846e35aa04d7daf8ce2b9f41ccf88b539f9
Carl Heinst <cheinst@gmail.com>
2020-04-17 18:00:25 +0100

    Simplifying code and updating readme

--- scripts/redeploy.groovy
+++ scripts/redeploy.groovy
@@ -14,7 +14,7 @@ pipeline {
                 }
             }
         }
-        stage('Rerun') {
+        stage('Final Run') {
             steps {
                 script {
                   build('loader/setup')
