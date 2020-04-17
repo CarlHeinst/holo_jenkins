120e92233b193f52e6c6926e3733639893d0f113
Carl Heinst <cheinst@gmail.com>
2020-04-17 17:40:10 +0100

    Simplifying code and updating readme

--- scripts/dsl_build.groovy
+++ scripts/dsl_build.groovy
@@ -40,30 +40,30 @@ multibranchPipelineJob('OVT/OVT-CLONE3') {
     }
 }
 
-// multibranchPipelineJob('OVT/OVT-CLONE4') {
-//     branchSources {
-//         git {
-//             id = 'admin'
-//             remote('https://github.com/tknerr/jenkins-pipes-helloworld.git')
-//         }
-//     }
-//     factory {
-//         workflowBranchProjectFactory {
-//             scriptPath('Jenkinsfile')
-//             }
-//     }
-// }
+multibranchPipelineJob('OVT/OVT-CLONE4') {
+    branchSources {
+        git {
+            id = 'admin'
+            remote('https://github.com/tknerr/jenkins-pipes-helloworld.git')
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile')
+            }
+    }
+}
 
-// multibranchPipelineJob('OVT/OVT-CLONE5') {
-//     branchSources {
-//         git {
-//             id = 'admin'
-//             remote('https://github.com/tknerr/jenkins-pipes-helloworld.git')
-//         }
-//     }
-//     factory {
-//         workflowBranchProjectFactory {
-//             scriptPath('Jenkinsfile')
-//             }
-//     }
-// }
+multibranchPipelineJob('OVT/OVT-CLONE5') {
+    branchSources {
+        git {
+            id = 'admin'
+            remote('https://github.com/tknerr/jenkins-pipes-helloworld.git')
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile')
+            }
+    }
+}
