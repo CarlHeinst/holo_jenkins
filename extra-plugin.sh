aa7c24895d9d044a04749ff8edcc95a0b211fd85
Carl Heinst <cheinst@gmail.com>
2020-04-20 17:04:43 +0100

    Simplifying code and updating readme

--- extra-plugin.sh
+++ extra-plugin.sh
@@ -1,3 +1,6 @@
+## This script can be used when you create an initial Jenkins instance to export all of the plugins that Jenkins recommends you should install.
+
 ## If running on Windows ensure you have choco install jq
 # Otherwise it will fail.
 curl.exe -s -k "http://admin:admin@localhost:8080/pluginManager/api/json?depth=1" | jq -r '.plugins[].shortName' | tee plugins.txt
+##
