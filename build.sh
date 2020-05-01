cb2df22135eb39e6c6b5b0e877d8c64f1248798c
Carl Heinst <cheinst@gmail.com>
2020-05-01 15:31:18 +0100

    Adding TOC

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker container rm holo-jenk -f
-docker build -t holo-jenkins .
+docker build --no-cache=true -t holo-jenkins .
 docker run -d -p 8080:8080 --env-file env.file --name=holo-jenk holo-jenkins
--- scripts/dsl_build.groovy
+++ scripts/dsl_build.groovy
@@ -1,3 +1,19 @@
+def create_pipeline_from_template(jobname, jenkinspath, repo_url, job_credential) {
+  multibranchPipelineJob(jobname) {
+    branchSources {
+        git {
+            id = job_credential
+            remote(repo_url)
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath(jenkinspath)
+            }
+    }
+  }
+}
+
 multibranchPipelineJob('OVT/OVT-CLONE1') {
     branchSources {
         git {
@@ -43,3 +59,7 @@ multibranchPipelineJob('REDEPLOY') {
 folder('crap')
 folder('crap1')
 folder('crap2')
+
+create_pipeline_from_template('OVT/OVT-CLONE7', 'Jenkinsfile', 'https://github.com/tknerr/jenkins-pipes-helloworld.git', 'admin')
+create_pipeline_from_template('OVT/OVT-CLONE8', 'Jenkinsfile', 'https://github.com/tknerr/jenkins-pipes-helloworld.git', 'admin')
+create_pipeline_from_template(jobname='OVT/OVT-CLONE9', jenkinspath='Jenkinsfile', repo_url='https://github.com/tknerr/jenkins-pipes-helloworld.git', job_credential='admin')
